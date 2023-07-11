72ad564799934460df0c24866784f7f852b54700
Cihat Şenel <70061200+cihatsnl34@users.noreply.github.com>
2023-07-11 17:27:47 +0300

    authController finished.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'prefix' => 'auth'
+],function(){
+    Route::post('login',[\App\Http\Controllers\AuthController::class,'login']);
+    Route::post('register',[\App\Http\Controllers\AuthController::class,'register']);
+});
+
+Route::group([
+    'middleware' => ['auth:api']
+],function(){
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class,'logout']);
+    Route::post('/authenticate', [\App\Http\Controllers\AuthController::class,'authenticate']);
+
 });
