688c2401f95043817fed5c5a8200bccc7722dcdf
Cihat Şenel <cihatsenell@gmail.com>
2023-07-18 02:19:54 +0300

    docker added.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,27 +9,24 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
-
-        // return response()->json([
-        //     'success' => true,
-        //     'request' => $request], 201);
+    public function register(Request $request)
+    {
         $request->validate([
-            'name'=>'required|string',
-            'email'=>'required|string|email|unique:users',
-            'password'=>'required|string'
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string'
         ]);
 
         $user = new User([
-            'name'=>$request->name,
-            'email'=>$request->email,
+            'name' => $request->name,
+            'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
         $user = $user->save();
-        
-        $credentials = ['email'=>$request->email, 'password'=>$request->password];
 
-        if(!Auth::attempt($credentials)) {
+        $credentials = ['email' => $request->email, 'password' => $request->password];
+
+        if (!Auth::attempt($credentials)) {
             return response()->json([
                 'message' => 'Giriş Yapilamadi Girilen Bilgileri Kontrol Ediniz.'
             ], 401);
@@ -37,7 +34,7 @@ class AuthController extends Controller
         $user = $request->user();
         $tokenResult = $user->createToken('Personal Access');
         $token = $tokenResult->token;
-        if($request->remember_me) {
+        if ($request->remember_me) {
             $token->expires_at = Carbon::now()->addWeeks(1);
         }
         $token->save();
@@ -51,54 +48,58 @@ class AuthController extends Controller
             'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
         ], 201);
     }
-    
-    public function login(Request $request) {
-       $request->validate([
-        'email' => 'required|string|email',
-        'password' => 'required|string',
-        'remember_me' => 'boolean'
-       ]);
 
-       $credentials = request(['email', 'password']);
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'remember_me' => 'boolean'
+        ]);
 
-       if(!Auth::attempt($credentials)) {
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Bilgiler hatalidir kontrol ediniz.'
+            ], 401);
+        }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personel Access Token');
+        $token = $tokenResult->token;
+        if ($request->remember_me) {
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        }
+        $token->save();
+
         return response()->json([
-            'message' => 'Bilgiler hatalidir kontrol ediniz.'
-        ], 401);
-       }
-
-       $user = $request->user();
-       $tokenResult = $user->createToken('Personel Access Token');
-       $token = $tokenResult->token;
-       if($request->remember_me) {
-        $token->expires_at = Carbon::now()->addWeeks(1);
-       }
-       $token->save();
-       
-       return response()->json([
-        'success' => true,
-        'id' => $user->id,
-        'name' => $user->name,
-        'email' => $user->email,
-        'access_token' => $tokenResult->accessToken,
-        'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
-    ], 201);
+            'success' => true,
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'access_token' => $tokenResult->accessToken,
+            'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateTimeString()
+        ], 201);
     }
 
-    public function logout(Request $request) {
-       $request->user()->token()->revoke();
-       return response()->json([
-        'message' => 'Cikis yapildi.'
-       ]);
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Cikis yapildi.'
+        ]);
     }
 
-    public function user(Request $request) {
+    public function user(Request $request)
+    {
         return response()->json($request->user());
     }
-    
-    public function authenticate(Request $request) {
+
+    public function authenticate(Request $request)
+    {
         $user = [];
-        if(Auth::check()) {
+        if (Auth::check()) {
             $user = $request->user();
         }
         return response()->json([
