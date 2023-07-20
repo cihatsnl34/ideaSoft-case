6193068a69458ab6e474c2b754277621140ff392
Cihat Şenel <cihatsenell@gmail.com>
2023-07-20 12:36:54 +0300

    removing unnecessary functions

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -103,6 +103,17 @@ class OrdersController extends Controller
         return response()->json(['message' => $orderResponse], 200);
     }
 
+    public function destroy($id)
+    {
+        $control = Order::where('id', $id)->count();
+        if ($control == 0) {
+            return response()->json(['success' => false, 'message' => 'No Order.']);
+        }
+        Order::where('id', $id)->delete();
+
+        return response()->json(['success' => true, 'message' => 'Deleted']);
+    }
+
     public function customerReport()
     {
         $orders = Order::all();
@@ -116,81 +127,5 @@ class OrdersController extends Controller
             $customerTotals[$customerId]['revenue'] = $totalAmount;
         }
         return response()->json($customerTotals, 200);
-
-        // $customerIdArray = [];
-        // $orders = Order::all();
-        // foreach ($orders as $orderKey => $orderValue) {
-        //     $customerIdArray[] = $orderValue->customerId;
-
-        // }
-        // return response()->json($orderValue, 200);
-    }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $control = Order::where('id', $id)->count();
-        if ($control == 0) {
-            return response()->json(['success' => false, 'message' => 'No Order.']);
-        }
-        Order::where('id', $id)->delete();
-
-        return response()->json(['success' => true, 'message' => 'Deleted']);
     }
 }
